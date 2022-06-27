cfb15369e16254e51db5612ba0ac59086c247d17
gggtongbiao <1223240438@qq.com>
2022-06-27 20:24:26 +0800

    下载了打包模板HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "dist"), //路径
     filename: "main.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
